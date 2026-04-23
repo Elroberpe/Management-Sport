eb1322a251ecc0c8e47d959fd5dce8a43c3e6b43
CarMorv <carlosmorenovi2000@gmail.com>
2026-04-22 19:56:26 -0500

    Creacionde kpiresponde

---
+++ src/main/java/com/sport/managementsport/dashboard/dto/KpiResponse.java
@@ -0,0 +1,14 @@
+package com.sport.managementsport.dashboard.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class KpiResponse {
+    private Number total;
+}
